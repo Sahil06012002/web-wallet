0fda5d215ab9e86d67988e58fc91be1ef6d916ab
Tushar Gagal <tushargagal1209@gmail.com>
2024-08-19 22:37:04 +0530

    commit

--- frontend/src/components/sol.tsx
+++ frontend/src/components/sol.tsx
@@ -11,40 +11,40 @@ interface keys {
   publicKey: string;
 }
 
-export default function SolWallet(prop : prop){
-    console.log(prop.seed)
-    // const [privateKey,setPrivateKey] = useState("");
-    const [keys,setKeys] = useState<keys[]>([]);
-    const [publicKey,setPublicKey] = useState<string[]>([]);
-    const [count,setCount] = useState(0);
-    function onClickHandler()
-    {
-        setCount(count +1)
-        const path = `m/44'/501'/${count}'/0'`;
-        if(prop.seed)
-        {
-            const derivedSeed = derivePath(path, prop.seed.toString("hex")).key;
-            const newPrivateKey = nacl.sign.keyPair.fromSeed(derivedSeed).secretKey;
-            const newPublicKey = Keypair.fromSecretKey(newPrivateKey).publicKey.toBase58()
-            const newKeyPair: keys = {
-                privateKey : newPrivateKey,
-                publicKey : newPublicKey
-            }
-            setKeys(prevKeys => [...prevKeys, newKeyPair]);
-            setPublicKey([...publicKey,newPublicKey ])
-        }
-        
+export default function SolWallet(prop: prop) {
+  console.log(prop.seed);
+  // const [privateKey,setPrivateKey] = useState("");
+  const [keys, setKeys] = useState<keys[]>([]);
+  const [publicKey, setPublicKey] = useState<string[]>([]);
+  const [count, setCount] = useState(0);
+  function onClickHandler() {
+    setCount(count + 1);
+    const path = `m/44'/501'/${count}'/0'`;
+    if (prop.seed) {
+      const derivedSeed = derivePath(path, prop.seed.toString("hex")).key;
+      const newPrivateKey = nacl.sign.keyPair.fromSeed(derivedSeed).secretKey;
+      const newPublicKey =
+        Keypair.fromSecretKey(newPrivateKey).publicKey.toBase58();
+      const newKeyPair: keys = {
+        privateKey: newPrivateKey,
+        publicKey: newPublicKey,
+      };
+      setKeys((prevKeys) => [...prevKeys, newKeyPair]);
+      setPublicKey([...publicKey, newPublicKey]);
     }
-    
-    return <div className="h100 w-100 border-black">
-        <button onClick={onClickHandler}>Add Solana Wallet</button>
-        <div>
+  }
+
+  return (
+    <div className="h100 w-100 border-black">
+      <button onClick={onClickHandler}>Add Solana Wallet</button>
+      <div>
         {keys.map((keyPair, index) => (
-        <div key={index}>
+          <div key={index}>
             <p>Public Key: {keyPair.publicKey}</p>
             <p>Private Key: {Array.from(keyPair.privateKey).toString()}</p>
-        </div>
+          </div>
         ))}
-</div>
+      </div>
     </div>
+  );
 }
