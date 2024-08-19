7f4e285cb3130ac30ae2bebedcf620b494f4e8bb
Tushar Gagal <tushargagal1209@gmail.com>
2024-08-19 14:52:15 +0530

    added etherium wallet

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,33 @@
-import './App.css'
+import "./App.css";
 import { generateMnemonic, mnemonicToSeedSync } from "bip39";
-import Sol from './components/sol';
-import Eth from './components/eth';
-import { useState } from 'react';
+import Sol from "./components/sol";
+import Eth from "./components/eth";
+import { useState } from "react";
 function App() {
-
   const [mnemonic, setMnemonic] = useState("");
   const [seed, setSeed] = useState<Buffer>();
 
   async function onClickHandler() {
-    const newMnemonic = generateMnemonic()
-    const newSeed = mnemonicToSeedSync(newMnemonic)
+    const newMnemonic = generateMnemonic();
+    const newSeed = mnemonicToSeedSync(newMnemonic);
 
     setMnemonic(newMnemonic);
     setSeed(newSeed);
   }
 
-  return <div className='text-center'>
-    <button className='my-5' onClick={onClickHandler}>Create Seed Phrase</button>
-    <div>
-    {mnemonic}
+  return (
+    <div className="text-center">
+      <button className="my-5" onClick={onClickHandler}>
+        Create Seed Phrase
+      </button>
+      <div>{mnemonic}</div>
+      <div>{seed}</div>
+      <div className="flex justify-around">
+        <Sol seed={seed}></Sol>
+        <Eth seed={seed}></Eth>
+      </div>
     </div>
-    <div>
-    {seed}
-    </div>
-    <div className='flex justify-around'>
-    <Sol seed={seed} ></Sol>
-    <Eth></Eth>
-    </div>
-
-  </div>
+  );
 }
 
-export default App
+export default App;
